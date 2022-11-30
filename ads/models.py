7bb51c32f91d2e036215a2b8bed0eadee7777c7e
Kislov Dmitry <kislov.urfu98@gmail.com>
2022-11-30 15:08:46 +0500

    unique

--- ads/models.py
+++ ads/models.py
@@ -16,7 +16,7 @@ class Ads(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
